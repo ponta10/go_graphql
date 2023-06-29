8bde6bb14ed84a0392b2b8238b457764f37d6dc9
ponta10 <mponta2002@gmail.com>
2023-06-29 12:46:38 +0900

    update graphqlURI

--- client/lib/apolloClient.ts
+++ client/lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:8084', // Replace with your GraphQL server URL
+  uri: 'http://localhost:8084/query', // Replace with your GraphQL server URL
   cache: new InMemoryCache(),
 });
 
