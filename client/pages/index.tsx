1e15c433606116bbc1ca47219938c9d30d6cb87b
ponta10 <mponta2002@gmail.com>
2023-06-30 14:12:20 +0900

    style center

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,17 +1,36 @@
 import { ApolloProvider } from '@apollo/client';
+import styled from 'styled-components';
 import apolloClient from '../lib/apolloClient';
 import TodoList from '../components/TodoList';
 import AddTodo from '../components/AddTodo';
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f5f5f5;
+`;
+
+const Box = styled.div`
+  width: 50%;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+`;
+
 const Home: React.FC = () => {
   return (
     <ApolloProvider client={apolloClient}>
-      <div>
-        <h2>Todoリスト</h2>
-        <TodoList />
-        <h2>Todo追加</h2>
-        <AddTodo />
-      </div>
+      <Container>
+        <Box>
+          <h2>Todoリスト</h2>
+          <TodoList />
+          <h2>Todo追加</h2>
+          <AddTodo />
+        </Box>
+      </Container>
     </ApolloProvider>
   );
 };
